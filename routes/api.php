b466d7bb8b63ed6d43c4adcbef26fd5da169c8a7
fabio-adaniya <f1hiroki@outlook.com>
2023-10-10 13:14:49 -0300

    Adição das rotas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\TelefoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/emails', [EmailController::class, 'index']);
+Route::get('/emails/{email}', [EmailController::class, 'show']);
+Route::post('/emails', [EmailController::class, 'store']);
+Route::put('/emails/{email}', [EmailController::class, 'update']);
+Route::delete('/emails/{email}', [EmailController::class, 'destroy']);
+Route::get('/telefones', [TelefoneController::class, 'index']);
+Route::get('/telefones/{telefone}', [TelefoneController::class, 'show']);
+Route::post('/telefones', [TelefoneController::class, 'store']);
+Route::put('/telefones/{telefone}', [TelefoneController::class, 'update']);
+Route::delete('/telefones/{telefone}', [TelefoneController::class, 'destroy']);
